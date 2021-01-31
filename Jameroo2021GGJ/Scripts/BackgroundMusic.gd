d52fdcd4da3f3f47c0c2a0e843e5945a9a849568
Andre Duvoisin <anduv13@gmail.com>
2021-01-31 13:32:23 -0600

    Add new caveloop layers, add fade-in.

--- Jameroo2021GGJ/Scripts/BackgroundMusic.gd
+++ Jameroo2021GGJ/Scripts/BackgroundMusic.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var FADE_IN_INITIAL_VOLUME = -80
+var FADE_IN_DURATION_SECONDS = 2
+var FADE_IN_DELAY_SECONDS = 0
+
 # temp for testing until we get a game timer that triggers this
 var time = 0
 func _process(delta):
@@ -11,8 +15,25 @@ func _process(delta):
 
 func play_layer_2():
 	var playbackPosition = $Layer1.get_playback_position()
-	$Layer2.play(playbackPosition)
+	fade_in_layer($Layer2, playbackPosition)
 
 func play_layer_3():
 	var playbackPosition = $Layer1.get_playback_position()
-	$Layer3.play(playbackPosition)
+	fade_in_layer($Layer3, playbackPosition)
+	
+func fade_in_layer(layer, playbackPosition):
+	var finalVolume = layer.volume_db
+	layer.volume_db = FADE_IN_INITIAL_VOLUME
+	
+	$VolumeTween.interpolate_property(
+		layer,
+		"volume_db",
+		FADE_IN_INITIAL_VOLUME,
+		finalVolume,
+		FADE_IN_DURATION_SECONDS,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN,
+		FADE_IN_DELAY_SECONDS);
+	$VolumeTween.start()
+	
+	layer.play(playbackPosition)
