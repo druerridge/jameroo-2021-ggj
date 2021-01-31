c6372ac785dd41d88d0ba9176643322a2bbffced
Andre Duvoisin <anduv13@gmail.com>
2021-01-31 15:30:16 -0600

    Add basic game timer impl for background music, fading light, end game.

--- Jameroo2021GGJ/Scripts/BackgroundMusic.gd
+++ Jameroo2021GGJ/Scripts/BackgroundMusic.gd
@@ -3,19 +3,16 @@ extends Node
 var FADE_IN_INITIAL_VOLUME = -80
 var FADE_IN_DURATION_SECONDS = 2
 var FADE_IN_DELAY_SECONDS = 0
-
-# temp for testing until we get a game timer that triggers this
-var time = 0
-func _process(delta):
-	time += delta
-	if time > 10 and not $Layer2.playing:
-		play_layer_2()
-	if time > 20 and not $Layer3.playing:
-		play_layer_3()
+		
+func is_layer_2_playing():
+	return $Layer2.playing
 
 func play_layer_2():
 	var playbackPosition = $Layer1.get_playback_position()
 	fade_in_layer($Layer2, playbackPosition)
+
+func is_layer_3_playing():
+	return $Layer3.playing
 
 func play_layer_3():
 	var playbackPosition = $Layer1.get_playback_position()
---
+++ Jameroo2021GGJ/Scripts/GameTimer.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var LEVEL_LENGTH_SECONDS = 30
+var VISIBLE_LIGHT_MINIMUM_SCALE = Vector2(0.2, 0.2)
+
+var elapsedTimeSeconds = 0
+var isFirstFrame = true
+
+var backgroundMusic
+var visibleLight: Node2D
+var visibleLightInitialScale: Vector2
+
+func _process(delta):
+	if isFirstFrame:
+		initialize()
+		isFirstFrame = false
+	
+	elapsedTimeSeconds += delta
+	
+	if elapsedTimeSeconds > LEVEL_LENGTH_SECONDS:
+		end_game()
+	else:
+		layer_background_music()
+		dim_lighting()
+
+func initialize():
+	# i'm sure there's a better way to do this
+	backgroundMusic = get_parent().get_node("BackgroundMusic")
+	visibleLight = get_parent().get_node("InteractiveTerrain").get_node("Player").get_node("Pivot").get_node("VisibleLight2D")
+	visibleLightInitialScale = visibleLight.scale
+
+func end_game():
+	print("end game!")
+
+func layer_background_music():
+	var oneThirdTime = LEVEL_LENGTH_SECONDS / 3
+	if elapsedTimeSeconds > oneThirdTime and not backgroundMusic.is_layer_2_playing():
+		backgroundMusic.play_layer_2()
+		
+	var twoThirdsTime = LEVEL_LENGTH_SECONDS / 3 * 2
+	if elapsedTimeSeconds > twoThirdsTime and not backgroundMusic.is_layer_3_playing():
+		backgroundMusic.play_layer_3()
+
+func dim_lighting():
+	visibleLight.scale = visibleLightInitialScale.linear_interpolate(VISIBLE_LIGHT_MINIMUM_SCALE, elapsedTimeSeconds / LEVEL_LENGTH_SECONDS)
